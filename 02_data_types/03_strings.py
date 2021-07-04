f796c09124d93993c2d4ea930943d2984b7edc94
Sudip Ghimire <ghimiresdp@gmail.com>
2021-07-04 16:40:07 +0545

    update 2

--- 02_data_types/03_strings.py
+++ 02_data_types/03_strings.py
@@ -18,105 +18,81 @@ Formatting strings:
 - f-strings
 
 """
-'this is a string'
-"this is a string"
 
-# single quotes
+# We can use Single quote for strings
+name = 'John'
 
-name_k = 'kusum'
-print(name_k)
+# We can use double quotes for strings
+name = "John"
 
-# double quotes
-name_r = "Rosan"
-print(name_r)
+# We can use single quotes inside double or vice versa
+x = "I am at John's"
+x = 'He said, "John is a cool guy!"'
 
-# single quotes inside double quotes
-cow = "Cow is an animal. cow's tail is so long."
+# If we want to have special characters or
+# We want single inside single quotes or double quotes inside double quotes, we have to use escape characters
 
+# We can achieve escape characters by using backslash `\` before the character.
+x = 'I am at John\'s'  # this will output :  I am at John's
 
-# escape characters
-cow = 'Cow is an animal. cow\'s tail is so long.'
-
-# some of the examples of escape characters
+# some of the examples of escape characters and their use cases are as follows:
 """
-\t -> tabs
-\n -> new lines
-\" -> double quotes
-\' -> single quotes
+\t => tabs
+\n => new lines
+\" => double quotes
+\' => single quotes
+\\ => backslash
+\a => ascii bell
+\b => backspace
+\r => carriage return
+\v => vertical tab
+for more detail about escape characters, please see https://docs.python.org
 """
 
-two_lines = "This is the first line.\nThis is\nthe second line."
-print(two_lines)
-
-hubby =  "knitting"
-
-hubby  =  "singing" 
-
+two_lines = "This is the first line.\nThis is the second line."
 
 paragraph = "Microsoft and our third-party\n vendors use cookies to store and access information such as unique IDs to deliver, maintain and improve our\n services and ads. If you agree, MSN and Microsoft Bing will personalize the content and ads that you see. You can select \"I Accept\" to consent to these uses or click on \"Manage preferences\" to review your options  You can change your selection under \"Manage\n Preferences\" at the bottom of this page.\n\n\n"
 
 print(paragraph)
 
-
-# please print your 2 hobbies with the same string but in the different lines using escape characters
-
-
-hobbies = "1. singing 2. knitting 3. singing 4. knitting 5. singing 6. knitting"
-print (hobbies)
-
-hobbies = "\n\n1.\tsinging\n2.\tknitting\n3.\tsinging\n4.\tknitting\n5.\tsinging\n6. knitting"
-print (hobbies)
-
+hobbies = "\n\n1.\tsinging\n2.\tdancing\n3.\tcoding"
+print(hobbies)
 
 # Concatenation
-# it is the process of adding 2 strings together
+# it is the process of adding or joining 2 strings together
 
 first_name = "Mohammad"
 last_name = "Ibrahim"
-last_name_2 = "Ali"
 
-name = "Mohammad " + "Ibrahim"
+full_name = "Mohammad" + "Ibrahim"  # MohammadIbrahim
 
-print("My name is " + name)
+print("My name is " + full_name)  # My name is MohammadIbrahim
 
-full_name = "My name is " + first_name + " " + last_name
-
-print("\n\n\n")
-
-print(full_name)
-full_name2 = "My name is " + first_name + " " + last_name_2
-print(full_name2)
-
+full_name = "My name is " + first_name + " " + last_name  # My name is Mohammad Ibrahim
 
 # length of the string
+"""
+we can get the length of string with `__len__()` method
+
+the method gives the length of string including all the white spaces and special characters
+"""
 
 length = paragraph.__len__()
 
-print("the length of the paragraph is " + str(length))
+print("the length of the paragraph is: " + str(length))
 
-print("\n\n")
-print("Mohammad".__len__())
-print("\n\n")
+print("Mohammad".__len__())  # 8
 
-print("kusum".__len__())
-print("kusum dahal".__len__()) 
+print("John Doe".__len__())  # 8
 
 # capitalization, Upper Case, and Lower Case
 
-print("kusum".capitalize()) # Kusum
+print("john".capitalize())  # John
 
-print("mohammad".upper())   # MOHAMMAD
-print("ROSHAN".lower()) # roshan
+print("john".upper())  # JOHN
+print("JOHN".lower())  # john
 
-# is decimal
-
+# isdecimal()
 value = "10"
-print(value.isdecimal())    # True
-
-
-value = "234eg"
-print(value.isdecimal())    # False
-
-
-
-# string formatting 
+print(value.isdecimal())  # True
+print("123stu".isdecimal())  # False
