f3f1965129da96388c810b8e50c6f47120ec3088
Sudip Ghimire <ghimiresdp@gmail.com>
2021-07-10 21:00:52 +0545

    string exercises added

--- practice/04.py
+++ practice/04.py
@@ -2,26 +2,55 @@
 © https://sudipghimire.com.np
 
 
-Hello world Program Exercise
+String Exercise
 
 
 Please read the note carefully and try to solve the problem below:
 
-1. Assign 55 to the variable `x` and print it in the console
-2. change the value of variable to be 60
-3. add x by 10 again
-
-note: please take the reference from the lab of datatypes "/src/02_data_types/01_variables.py"
-note: please try to print the value of the variable in each and every steps or questions
 
 """
 
-#  answer 1
+"""
+1. Create two variablrs first_name, and last_name and print the sentence in the format below:
+   suppose first_name is John and last_name is Doe
+   "My name is John Doe"
 
-#  answer 2
+   a. use + operator to concatenate strings
+   b. use format() method to achieve the same result
+   c. use f-strings to achieve the same result
+   d. use %s formatting method to achieve the same result
+"""
+# answer 1
+# 1.a
+first_name = ""
 
-#  answer 3
 
-#  answer 4
+# 1.b
 
-#  answer 5
+
+# 1.c
+
+
+# 1.d
+
+
+"""
+2. Assign a variable  pi and assign value 3.14159265
+    a. use formatting strings to show pi with 3 digits after the decimal
+    b. use formatting strings to show pi with 2 digits after the decimal but
+       allocate 10 spaces for the variable
+    c. use f-string to show the result in the following format:
+        "The value of PIE is        3.14"
+
+        hint: "%<a>.<b>f"
+"""
+# answer 2
+# a
+
+
+# b
+
+
+# c
+
+
