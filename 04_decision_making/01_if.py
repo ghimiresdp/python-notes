a379f27a5a93b23d0fc99d2039aee164b249d4f0
Sudip Ghimire <ghimiresdp@gmail.com>
2021-07-24 06:52:13 +0545

    more on conditionals

--- 04_decision_making/01_if.py
+++ 04_decision_making/01_if.py
@@ -15,10 +15,12 @@ if <condition>:
     <statements>
 ```
 """
+# %%
 a = 20
 b = 25
-if a < b:
+if a > b:
     print(f'{a} is less than {b}')
+print('dummy line')
 
 # %% [markdown]
 """
@@ -26,7 +28,7 @@ if a < b:
 - if else condition is same as if condition
 - we use else for the condition that does not satisfy the if condition
 
-Syntax
+Structure
 ```
 if <condition>:
     <statements>
@@ -49,7 +51,7 @@ else:
   elif.
 - we can use any number of elif conditions in python.
 
-Syntax
+Structure
 ```
 if <condition>:
     <statements>
@@ -68,12 +70,57 @@ else:
     print(f'{x} is greater than 5')
 
 # %%  if else as one liner assignment
+# They are also called as ternary operators
 # <value_1> if <condition> else <value_2>
 a = False
 x = 5 if a is True else 10
 print(x)
+# %%
+a = 11
+x = 'even' if a % 2 == 0 else 'odd'
+
+print(x)
+
+# I have a note of <> amount.
+# I need to give it to Mohammad if
+# it is greater than 100, and Roshan if it is less than 100
+amount = 50
+
+# Receiver is Mohammad if amount is greater than 100 else Roshan
+receiver = 'Mohammad' if amount > 100 else 'Roshan'
+
+"""
+# The regular condition would be
+if amount > 100:
+    receiver = 'Mohammad'
+else:
+    receiver = 'Roshan'
+"""
 
 # %% <value_1> if <condition_1> else <value_2> if <condition_2> else <value_3?
 a = 11
+# The regular condition would be
+if a < 10:
+    x = 'small'
+elif a == 10:
+    x = 'medium'
+else:
+    x = 'large'
+
 x = 'small' if a < 10 else 'medium' if a == 10 else 'large'
+
 print(x)
+
+# %%
+a = 'Brazil'
+# The regular condition would be
+if a == 'Brazil':
+    continent = 'Americas'
+elif a == 'Spain':
+    continent = 'Europe'
+else:
+    continent = 'Asia'
+
+continent = 'Americas' if a == 'Brazil' else 'Europe' if a == 'Spain' else 'Asia'
+
+print(continent)
