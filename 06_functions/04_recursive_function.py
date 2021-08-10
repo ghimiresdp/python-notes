0ed6eaaf313a57703f4193dccdd37b85107dc3f6
Sudip Ghimire <ghimiresdp@gmail.com>
2021-08-10 08:16:19 +0545

    recursive and lambda

---
+++ 06_functions/04_recursive_function.py
@@ -0,0 +1,100 @@
+# %%
+"""
+Recursive Function
+- A function that calls itself
+- it contains 2 different elements
+    - recursion limit
+    - next recursion
+
+Basic Syntax
+
+def <function_name>(<depth>):        # depth might be the value to recurse
+    if <condition>:
+        <function_name>(<updated_depth>)
+    else:                           # recursion Limit
+        return <final value>
+"""
+
+
+# Program to convert decimal to binary
+
+
+def dec2bin(n: int):
+    if n != 0:
+        return dec2bin(n // 2) * 10 + n % 2
+    else:
+        return 0
+
+
+print(dec2bin(14))
+
+##
+
+x = 14
+"""
+1. x = 14
+
+# starts expanding
+
+2. dec2bin(14)
+3. dec2bin(dec2bin(7) * 10 + 0)
+4. dec2bin(dec2bin(3) * 10 + 1) * 10 + 0)
+5. dec2bin(dec2bin(dec2bin(1) * 10 + 1) * 10 + 1) * 10 + 0)
+6. dec2bin(dec2bin(dec2bin(dec2bin(0) * 10 + 1 ) * 10 + 1) * 10 + 1) * 10 + 0)
+
+# starts simplifying
+
+7. dec2bin(dec2bin(dec2bin(0 * 10 + 1 ) * 10 + 1) * 10 + 1) * 10 + 0)
+8. dec2bin(dec2bin(1 * 10 + 1) * 10 + 1) * 10 + 0)
+9. dec2bin(11 * 10 + 1) * 10 + 0)
+10. dec2bin(111) * 10 + 0)
+10. 111 * 10 + 0)
+11. 1110
+"""
+
+# %%
+
+"""
+similar result without recursion
+"""
+
+
+def dec2bin2(n: int):
+    result = []
+    next1 = n
+    while next1 != 0:
+        result.append(str(next1 % 2))
+        next1 = next1 // 2
+    result.reverse()
+    return ''.join(result)
+
+
+# %%
+"""
+recursive function to find out factorial of a number
+
+example: 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+"""
+
+# without recursion:
+
+x = 5
+result = x
+while x > 1:
+    result = result * (x - 1)
+    x = x - 1
+print(result)
+
+
+# if x = 0 0! = 1
+# %% with recursion
+
+def factorial(x: int):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x - 1)
+
+
+print(factorial(5))
---
+++ 06_functions/05_lambda.py
@@ -0,0 +1,65 @@
+# © https://sudipghimire.com.np
+"""
+Python Lambdas
+- lambdas are also known as anonymous functions
+- they are tiny functions which contains one statement
+- lambdas are written in a single line
+- they can have zero or more arguments
+- they are useful when we have to do small repetitive tasks
+
+structure of lambda
+<function_name> = lambda <arg>,<arg>, ...: <return_statement>
+
+- everything before `:` and after `lambda` are arguments
+- expression after `:` is the return value
+example:
+f1 = lambda a,b: a+b
+f2 = lambda a: a**2
+f3 = lambda : print("Hey There!")
+"""
+
+
+# %% regular function to add 2 numbers and return value
+
+def add_numbers(a, b):
+    return a + b
+
+
+add_numbers(4, 5)
+
+# %% same operation with lambda
+
+add_num = lambda a, b: a + b
+add_num(4, 5)
+
+# %%
+# multiply 2 numbers
+a = 6
+b = 8
+e = 10
+
+multiply = lambda c, d: c * d + e
+
+multiply(a, b)
+# %% lambda with no arguments
+
+
+print_hello = lambda: print("hello world")
+print_hello()
+
+# %% conditionals inside lambda
+
+# ternary operator "Even" if x % 2 == 0 else 'Odd'
+
+odd_even = lambda x: "Even" if x % 2 == 0 else 'Odd'
+
+print(odd_even(14))
+
+# %% double the list
+lst = [1, 2, 3, 4, 5]
+
+double1 = lambda l: [x * 2 for x in l]
+
+double1(lst)
+
+lst = [x * 2 for x in lst]
