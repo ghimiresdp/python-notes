92545f0accfa58db63116751a9d18de4a924ad8c
Sudip Ghimire <ghimiresdp@gmail.com>
2022-03-06 21:38:36 +0545

    chapter 1 quiz solutions

---
+++ c01_basics/quiz_solution/q0102.py
@@ -0,0 +1,28 @@
+"""
+© https://sudipghimire.com.np
+
+1. Print "Hello World" using print() function
+2. Comment the line from the above solution and try running the program again. Explain what happens and why it happens
+3. Create a multiline comment using Triple single and double quotes and add your content
+
+"""
+# Answers
+
+# 1
+print("Hello World")
+
+#2
+"""
+When above line is commented, Nothing gets printed since the comment line is ignored by the interpreter.
+"""
+
+# 3
+"""
+This is a multiline comment
+created using triple double-quotation marks
+"""
+
+'''
+This is a multiline comment
+created using triple single-quotation marks
+'''
