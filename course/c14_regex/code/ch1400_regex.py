f50ce577306071869c22a4abf6bb00286e8681b1
Sudip Ghimire <ghimiresdp@gmail.com>
2022-05-03 07:27:38 +0545

    `re.match()` example

--- course/c14_regex/code/ch1400_regex.py
+++ course/c14_regex/code/ch1400_regex.py
@@ -9,16 +9,14 @@ pattern = re.compile(r'^A\w*a$')
 for country in countries:
     print(pattern.match(country))
 
-
 # compile example 2
 
-strings = ["0xaa", 'FA04', 'Ak45','As40', '0x5h', '0x56']
+strings = ["0xaa", 'FA04', 'Ak45', 'As40', '0x5h', '0x56']
 
 pat = re.compile(r'^(0x)?[0-9A-Fa-f]+$')
 
 for st in strings:
     print(pat.match(st))
-
 
 # search()
 text = "Dear sir, I've emailed you last week regarding the assignment. I've also sent you another mail specifying the next assignment."
@@ -26,7 +24,13 @@ text = "Dear sir, I've emailed you last week regarding the assignment. I've also
 results = re.search(r'e?mail(ed)?', text)
 print(results)
 
-
+# match()
+text = 'Dear sir'
+print(re.match(r'[a-z]e\w+', text, re.I))
+"""
+# output
+<re.Match object; span=(0, 4), match='Dear'>
+"""
 
 # # findall
 # countries = 'USA, Japan, Angola, China, Algeria, Nepal, Argentina, Albania'
