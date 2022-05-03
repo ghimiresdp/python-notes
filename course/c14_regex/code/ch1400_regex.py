95c8af238885a3e1947540445055791c88159f73
Sudip Ghimire <ghimiresdp@gmail.com>
2022-05-03 08:05:39 +0545

    `re.sub()` examples

--- course/c14_regex/code/ch1400_regex.py
+++ course/c14_regex/code/ch1400_regex.py
@@ -9,6 +9,19 @@ pattern = re.compile(r'^A\w*a$')
 for country in countries:
     print(pattern.match(country))
 
+"""
+# Output
+
+None
+None
+<re.Match object; span=(0, 6), match='Angola'>
+None
+<re.Match object; span=(0, 7), match='Algeria'>
+None
+<re.Match object; span=(0, 9), match='Argentina'>
+<re.Match object; span=(0, 7), match='Albania'>
+"""
+
 # compile example 2
 
 strings = ["0xaa", 'FA04', 'Ak45', 'As40', '0x5h', '0x56']
@@ -18,17 +31,35 @@ pat = re.compile(r'^(0x)?[0-9A-Fa-f]+$')
 for st in strings:
     print(pat.match(st))
 
+"""
+# output
+
+<re.Match object; span=(0, 4), match='0xaa'>
+<re.Match object; span=(0, 4), match='FA04'>
+None
+None
+None
+<re.Match object; span=(0, 4), match='0x56'>
+"""
+
 # search()
 text = "Dear sir, I've emailed you last week regarding the assignment. I've also sent you another mail specifying the next assignment."
 
 results = re.search(r'e?mail(ed)?', text)
 print(results)
 
+"""
+# Output
+
+<re.Match object; span=(15, 22), match='emailed'>
+"""
+
 # match()
 text = 'Dear sir'
 print(re.match(r'[a-z]e\w+', text, re.I))
 """
 # output
+
 <re.Match object; span=(0, 4), match='Dear'>
 """
 
@@ -47,3 +78,16 @@ print(re.findall(r'A[a-z]+a', countries))
 # split
 countries = 'USA, Japan; Angola! China, Algeria% Nepal; Argentina/ Albania'
 print(re.split(r'\W+', countries))
+"""
+# Output:
+
+['USA', ' Japan', ' Angola', ' China', ' Algeria', ' Nepal', ' Argentina', ' Albania']
+"""
+
+
+print(re.sub(r'\W+', ', ', countries))
+"""
+# Output
+
+USA, Japan, Angola, China, Algeria, Nepal, Argentina, Albania
+"""
