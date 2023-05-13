b2283a30fed3639832df9d1f4ea3e11b638159d9
Sudip Ghimire <ghimiresdp@gmail.com>
2023-05-13 23:02:41 +0545

    feat: assignment 1

---
+++ assignments/assignment_001.py
@@ -0,0 +1,86 @@
+# ===============================[ Assignment 1 ]===============================
+"""
+================================================================================
+This assignment is targeted to the student who have completed the following
+chapters:
+1. Introduction
+2. Basic Data Types
+
+Please read the instructions carefully before you answer the question.
+
+- Ensure you have completed all the requirements of the assignment.
+- Make sure your work is legible and easy to read.
+- Check for spelling and grammatical errors.
+- Write your answers only in the specified part to avoid errors.
+
+================================================================================
+"""
+
+# =================================[ TASK 01 ]=================================
+"""
+Example:
+1. Create a variable called `name` and assign the value `John Doe` to it.
+"""
+
+name = "John Doe"
+
+"""
+2. Now create a variable named `age` and assign an integer value to it.
+   Use F-string to generate a sentence that explains the name and age.
+
+   Eg: Hi! My name is John Doe and I am 20 years old.
+"""
+# -----------------------------[ YOUR ANSWER HERE ]----------------------------
+
+
+# =================================[ TASK 02 ]=================================
+
+"""
+An area of a rectangle is calculated using the formula as follows:
+
+        area = length * width
+
+1. Create variables named `length` and `width` and assign floating point values
+   to them.
+2. Find out the area of a rectangle and assign it to the variable area_1.
+3. perform explicit typecasting of variables `length` and `width` and find out
+   the area of the rectangle and assign it to the variable area_2
+4. Check the difference between `area_1` and `area_2` and explain why explicit
+   typecasting is affecting the original value of the area.
+"""
+# -----------------------------[ YOUR ANSWER HERE ]----------------------------
+
+
+# =================================[ TASK 03 ]=================================
+"""
+The height of Mount Everest was 8848.0 meters. Now, the height of Mt. Everest is
+now 8848.6 meters. Please check whether the previous value is smaller than the
+current value using relational operator ( <, ==, >, <=, or >= ).
+"""
+# -----------------------------[ YOUR ANSWER HERE ]----------------------------
+
+
+# =================================[ TASK 03 ]=================================
+"""
+In Geometry, the distance between two points is calculated using the formula
+below:
+
+        distance = ( (x2 - x1)**2 + (y2 - y1)**2 )**0.5
+
+1. x1, x2, y1, y2 are integer points.
+2. Assign (x1, y1) and (x2, y2) using  multiple assignment. ( eg: x, y = 0, 2 )
+3. Square root is represented by an exponentiation by 0.5 or (1/2)th power of
+   the number.
+4. The final result or the distance is a floating point number.
+
+
+Please calculate the distance between two points and print the result and assign
+it to the variable named `distance` and print the following statement:
+
+        The distance between the points (1,1) and (4,5) is 5.0
+
+"""
+# -----------------------------[ YOUR ANSWER HERE ]----------------------------
+
+
+# =============================================================================
