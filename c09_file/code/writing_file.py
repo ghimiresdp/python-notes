b1d8a1be2aa7680cd8415e67b2ccff3ce79200c9
Sudip Ghimire <ghimiresdp@gmail.com>
2024-09-07 01:46:52 +0545

    code: writing file

---
+++ c09_file/code/writing_file.py
@@ -0,0 +1,28 @@
+# FIXME: please use this path if you run this script from current directory
+# file_name = "file_1.txt"
+
+# ! Please create a new file named `file1.txt` in this directory to open the file
+
+# Please use this path if you run this script from the project directory
+file_name = "c09_file/code/file2.txt"
+
+content = """This is a content to the written file.
+- This is a point 1
+- This is a point 2
+"""
+
+# ------------------------------------------------------------------------------
+# opening the file in writing mode
+file = open(file_name, "w")
+
+file.write(content)
+
+# we need to close the file once we don't need anymore
+file.close()
+
+# ------------------------------------------------------------------------------
+# using with statement
+with open(file_name, "w") as file:
+    file.write(content)
+    # we need to close the file once we don't need anymore
+    file.close()
